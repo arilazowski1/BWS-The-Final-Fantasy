94faa51efd931815e71e174091125976f1056597
arilazowski1 <100659155+arilazowski1@users.noreply.github.com>
2022-03-01 04:09:49 -0800

    Update index.svelte

--- src/routes/constitution/index.svelte
+++ src/routes/constitution/index.svelte
@@ -153,7 +153,7 @@
     
     <h3 class="subSectionHeading" bind:this={oneOne}>1.1 Positional Breakdown</h3>
     
-    <p>28 Total Players</p>
+    <p>18 Total Players</p>
     
     <p class="underscore">Starters</p>
     <ul>
